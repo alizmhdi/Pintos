a784611fa23f8f3c4ecf8f1242ba7bc6aceeeada
Pasha <alipasha.montaseri@gmail.com>
2023-03-09 12:54:13 +0000

    fix typo

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -90,7 +90,7 @@ struct process_status {
     struct semaphore sema;
     struct list_elem elem;
     /* we define this variable to dedicate number of threads that access to this status and use it for free space
-       at the begining this variable set to 2 (parent anc child) */
+       at the begining this variable set to 2 (parent and child) */
     int ref_count;
     /* for locking ref_count for Avoiding race condition */
     struct lock lock; 
