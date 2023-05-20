19fc28c5bca93100eb33b125d6fd9899372d9052
soroush <soroush.sherafat02@sharif.edu>
2023-05-20 19:59:53 +0000

    added basic functions to `cache.c`

--- pintos/src/filesys/cache.h
+++ pintos/src/filesys/cache.h
@@ -1,22 +1,23 @@
 #ifndef FILESYS_CACHE_H
 #define FILESYS_CACHE_H
 
+#include "off_t.h"
 #include "devices/block.h"
-#include "threads/synch.h"
 #include "lib/kernel/list.h"
+#include "threads/synch.h"
 
 #define CACHE_SIZE 64
 
 
 typedef struct cache_block
-{
-  block_sector_t sector_index;
-  char data[BLOCK_SECTOR_SIZE];
-  bool valid;
-  bool dirty;
-  struct list_elem elem; 
-  struct lock block_lock;
-} cache_block_t;
+  {
+    block_sector_t sector_index;
+    char data[BLOCK_SECTOR_SIZE];
+    bool is_valid;
+    bool is_dirty;
+    struct list_elem elem; 
+    struct lock cache_lock;
+  } cache_block_t;
 
 
 cache_block_t cache_blocks[CACHE_SIZE];
@@ -24,5 +25,14 @@ cache_block_t cache_blocks[CACHE_SIZE];
 struct list cache_list;
 
 struct lock cache_lock;
+
+
+void cache_init (void);
+
+void cache_write (struct block *fs_device, block_sector_t sector_idx, void *source, off_t offset, int chunk_size);
+
+void cache_read (struct block *fs_device, block_sector_t sector_idx, void *destination, off_t offset, int chunk_size);
+
+void cache_flush (struct block *fs_device);
 
 #endif /* FILESYS_CACHE_H */
