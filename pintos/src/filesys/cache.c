296a51868c800d6adce0b816a9a6cc287b76d714
Kian <kkibian@gmail.com>
2023-05-23 07:30:23 +0000

    FIXED cache, added cache_shutdown() to filesys_done()

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -1,23 +1,7 @@
-#include <string.h>
 #include <debug.h>
+#include <string.h>
 #include "cache.h"
-#include "filesys.h"
-
-/* Defines used for updating cache stats. */
-#define hit 1
-#define miss 0
-#define access -1
-
-static cache_block_t cache_blocks[CACHE_SIZE];
-static struct list cache_list;
-static struct lock cache_lock;
-
-static int cache_initialized = 0;
-
-static long long hit_count;
-static long long miss_count;
-static long long access_count;
-static struct lock stat_lock;
+#include "filesys/filesys.h"
 
 static void
 stat_update (int mode)
@@ -25,41 +9,37 @@ stat_update (int mode)
   lock_acquire (&stat_lock);
   switch (mode)
   {
-  case hit:
+  case HIT:
     hit_count++;
     break;
   
-  case miss:
+  case MISS:
     miss_count++;
-    break;
-
-  case access:
-    access_count++;
     break;
   }
   lock_release (&stat_lock);
 }
 
 /* Function used to initialize the cache. */
-void
-cache_init (void)
+void 
+cache_init(void)
 {
   /* Initialize the locks. */
-  lock_init (&cache_lock);
+  lock_init(&cache_lock);
   lock_init (&stat_lock);
-  
-  /* Initialize the blocks. */
-  lock_acquire (&cache_lock);
-  list_init (&cache_list);
-  for (int i = 0; i < CACHE_SIZE; i++)
-    {
-      lock_init (&cache_blocks[i].block_lock);
-      cache_blocks[i].is_dirty = false;
-      cache_blocks[i].is_valid = false;
-      list_push_back (&cache_list, &cache_blocks[i].elem);
-    }
 
-  lock_release (&cache_lock);
+  /* Initialize the blocks. */
+  lock_acquire(&cache_lock);
+  list_init(&cache_list);
+  for (int i = 0; i < CACHE_SIZE; i++)
+  {
+    lock_init(&cache_blocks[i].block_lock);
+    cache_blocks[i].is_dirty = false;
+    cache_blocks[i].is_valid = false;
+    list_push_back(&cache_list, &cache_blocks[i].elem);
+  }
+
+  lock_release(&cache_lock);
 
   /* Initialize the stats. */
   lock_acquire (&stat_lock);
@@ -68,109 +48,125 @@ cache_init (void)
   access_count = 0;
   lock_release (&stat_lock);
 
-  cache_initialized = 1;
-}
-
-/* Write back the cache block if is_dirty, then set is_valid and is_dirty. */
-void 
-cache_evict (struct block *fs_device, struct cache_block *cache_block)
-{
-  ASSERT (lock_held_by_current_thread (&cache_block->block_lock));
-  if (cache_block->is_valid && cache_block->is_dirty)
-  {
-    block_write (fs_device, cache_block->sector_index, cache_block->data);
-  }
-
-  cache_block->is_dirty = false;
-  cache_block->is_valid = false;
+  cache_initialized = true;
 }
 
 /* Find cache block with current sector index, acquire the lock and return the block.
   If no blocks found, evict least recently used block from cache list, acquire its lock and return the block.*/
-static struct cache_block *
-get_cache_block (struct block *fs_device, block_sector_t sector_idx)
+struct cache_block *
+get_cache_block(struct block *fs_device, block_sector_t sector_idx)
 {
-  stat_update (access);
+  lock_acquire(&cache_lock);
 
   for (int i = 0; i < CACHE_SIZE; i++)
   {
     lock_acquire (&cache_blocks[i].block_lock);
+
     /* Cache hit case*/
-    if (cache_blocks[i].sector_index == sector_idx && cache_blocks[i].is_valid)
+    if (cache_blocks[i].is_valid && cache_blocks[i].sector_index == sector_idx)
     {
-      lock_acquire (&cache_lock);
-      list_remove (&cache_blocks[i].elem);
-      list_push_back (&cache_list, &cache_blocks[i].elem);
-      lock_release (&cache_lock);
+      list_remove(&cache_blocks[i].elem);
+      list_push_back(&cache_list, &cache_blocks[i].elem);
 
-      stat_update (hit);
-
+      lock_release(&cache_lock);
+      stat_update(HIT);
       return &cache_blocks[i];
     }
+    
     lock_release (&cache_blocks[i].block_lock);
   }
 
   /* Cache miss case*/
-  stat_update (miss);
-  lock_acquire (&cache_lock);
-  struct cache_block *lru_block = list_entry(list_pop_front (&cache_list), struct cache_block, elem);
-  lock_acquire (&lru_block->block_lock);
+  struct cache_block *lru_block = list_entry(list_pop_front(&cache_list), struct cache_block, elem);
+  lock_acquire(&lru_block->block_lock);
 
-  cache_evict (fs_device, lru_block);
-  ASSERT (!lru_block->is_dirty && !lru_block->is_valid);
-  block_read (fs_device, sector_idx, lru_block->data);
+  flush_block(fs_device, lru_block);
+  block_read(fs_device, sector_idx, lru_block->data);
   lru_block->sector_index = sector_idx;
   lru_block->is_valid = true;
-  list_push_back (&cache_list, &lru_block->elem);
-  lock_release (&cache_lock);
+  list_push_back(&cache_list, &lru_block->elem);
 
-  return &lru_block;
+  lock_release(&cache_lock);
+  stat_update(MISS);
+  return lru_block;
 }
 
-void
+void 
 cache_write(struct block *fs_device, block_sector_t sector_idx, void *source, off_t offset, int chunk_size)
 {
-  ASSERT (cache_initialized);
-  ASSERT (fs_device != NULL);
-  ASSERT (offset + chunk_size <= BLOCK_SECTOR_SIZE);
+  ASSERT(cache_initialized);
+  ASSERT(fs_device != NULL);
+  ASSERT(offset + chunk_size <= BLOCK_SECTOR_SIZE);
 
-  struct cache_block *cache_block =  get_cache_block (fs_device, sector_idx);
-  ASSERT (lock_held_by_current_thread (&cache_block->block_lock));
-  ASSERT (cache_block->is_valid);
-  memcpy (&cache_block->data + offset, source, chunk_size);
+  struct cache_block *cache_block = get_cache_block(fs_device, sector_idx);
+  ASSERT(lock_held_by_current_thread(&cache_block->block_lock));
+  ASSERT(cache_block->is_valid);
+  memcpy(cache_block->data + offset, source, chunk_size);
   cache_block->is_dirty = true;
-  lock_release (&cache_block->block_lock);
+  lock_release(&cache_block->block_lock);
 }
 
-void
-cache_read (struct block *fs_device, block_sector_t sector_idx, void *destination, off_t offset, int chunk_size)
+void 
+cache_read(struct block *fs_device, block_sector_t sector_idx, void *destination, off_t offset, int chunk_size)
 {
-  ASSERT (cache_initialized);
-  ASSERT (fs_device != NULL);
-  ASSERT (offset + chunk_size <= BLOCK_SECTOR_SIZE);
+  ASSERT(cache_initialized);
+  ASSERT(fs_device != NULL);
+  ASSERT(offset + chunk_size <= BLOCK_SECTOR_SIZE);
 
-  struct cache_block *cache_block =  get_cache_block (fs_device, sector_idx);
-  ASSERT (lock_held_by_current_thread (&cache_block->block_lock));
-  ASSERT (cache_block->is_valid);
+  struct cache_block *cache_block = get_cache_block(fs_device, sector_idx);
+  ASSERT(lock_held_by_current_thread(&cache_block->block_lock));
+  ASSERT(cache_block->is_valid);
 
-  memcpy (destination, cache_block->data + offset, chunk_size);
-  lock_release (&cache_block->block_lock);
+  memcpy(destination, cache_block->data + offset, chunk_size);
+  lock_release(&cache_block->block_lock);
 }
 
-/* Evict all cache blocks and invalidate cache. */
-void
-cache_flush (struct block *fs_device)
+void 
+flush_block(struct block *fs_device, struct cache_block *cache_block)
 {
-  ASSERT (fs_device != NULL);
+  if (cache_block->is_valid && cache_block->is_dirty)
+  {
+    block_write(fs_device, cache_block->sector_index, cache_block->data);
+  }
 
-  if (!cache_initialized)
-    return;
+  cache_block->is_dirty = false;
+}
 
+/* (Not yet) used for write-behind functionality. */
+void 
+cache_flush(struct block *fs_device)
+{
   for (int i = 0; i < CACHE_SIZE; i++)
   {
-    lock_acquire (&cache_blocks[i].block_lock);
-    cache_evict (fs_device, &cache_blocks[i]);
-    lock_release (&cache_blocks[i].block_lock);
+    flush_block (fs_device, &cache_blocks[i]);
   }
 }
 
+/* Flush and invalidate all cache blocks.*/
+void 
+cache_shutdown(struct block *fs_device)
+{
+  for (int i = 0; i < CACHE_SIZE; i++)
+  {
+    flush_block (fs_device, &cache_blocks[i]);
+    cache_blocks[i].is_valid = false;
+  }
+}
+
+/* To be used in cache stat syscalls. */
+size_t
+cache_count (int mode)
+{
+  lock_acquire(&stat_lock);
+  switch (mode)
+  {
+  case HIT:
+    return hit_count;
+    break;
+  
+  case MISS:
+    return miss_count;
+    break;
+  }
+  lock_release(&stat_lock);
+}
--- pintos/src/filesys/cache.h
+++ pintos/src/filesys/cache.h
@@ -8,6 +8,10 @@
 
 #define CACHE_SIZE 64
 
+/* Defines used for updating cache stats. */
+#define HIT 1
+#define MISS 0
+
 
 typedef struct cache_block
   {
@@ -19,12 +23,27 @@ typedef struct cache_block
     struct lock block_lock;
   } cache_block_t;
 
+cache_block_t cache_blocks[CACHE_SIZE];
+struct list cache_list;
+struct lock cache_lock;
+
+bool cache_initialized;
+
+long long hit_count;
+long long miss_count;
+long long access_count;
+struct lock stat_lock;
+
 void cache_init (void);
 
 void cache_write (struct block *fs_device, block_sector_t sector_idx, void *source, off_t offset, int chunk_size);
 
 void cache_read (struct block *fs_device, block_sector_t sector_idx, void *destination, off_t offset, int chunk_size);
 
+void flush_block (struct block *fs_device, struct cache_block *cache_block);
+
 void cache_flush (struct block *fs_device);
+
+void cache_shutdown (struct block *fs_device);
 
 #endif /* FILESYS_CACHE_H */
