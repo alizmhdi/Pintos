ba5a08d96ad73c186a075c87e03d5cdd0f90f6b7
soroush <soroush.sherafat02@sharif.edu>
2023-05-24 15:51:43 +0000

    corrected `bf-hit` test

--- pintos/src/tests/filesys/extended/bf-hit.c
+++ pintos/src/tests/filesys/extended/bf-hit.c
@@ -1,88 +1,52 @@
-/* ASSERT_CONDITIONs growing of hit rate after a file has been accessed.
-   To ASSERT_CONDITION this, a file is created with random data, and hit
-   rate is calculated twice for accessing. The hit rate should
-   grow in the second run. */
-
-#include <syscall.h>
 #include <random.h>
+#include <syscall.h>
 #include "tests/lib.h"
 #include "tests/main.h"
 
-#define FILE_SIZE 8096
-#define BLOCK_SIZE 512
+#define BUF_SIZE 16384
 
-/* from `cache.h` */
 #define HIT 1
 #define MISS 0
+
+static char buf[BUF_SIZE];
 
 void
 test_main (void)
 {
-  char file_buffer[FILE_SIZE];
-  int res;
+  int fd;
+  
+  invalidate_cache ();
+  msg ("invalidate cache");
+ 
+  CHECK (create ("a", BUF_SIZE), "create \"a\"");
+  CHECK ((fd = open ("a")) > 1, "open \"a\"");
 
-  res = create ("a", FILE_SIZE);
-  ASSERT_CONDITION (res > 0, "failed to create a file called a.");
-  msg ("created file named 'a'.");
-
-  int fd = open("a");
-  ASSERT_CONDITION (fd > 2, "failed to open the file, expected a `fd` value greater than 2 but got: '%d'.", fd);
-  msg ("opened the file.");
-
-  random_init (3853);
-  random_bytes (file_buffer, FILE_SIZE - 1);
-  file_buffer[FILE_SIZE - 1] = '\0';
-  res = write (fd, file_buffer, FILE_SIZE);
-  ASSERT_CONDITION (res == FILE_SIZE, "failed to write to file, expected to write '%d' bytes but only got '%d' bytes.", FILE_SIZE, res);
-  msg ("wrote random bytes to file.");
+  random_init (2020);
+  random_bytes (buf, sizeof(buf));
+  CHECK (write (fd, buf, sizeof(buf)) == BUF_SIZE, "write to \"a\"");
 
   close (fd);
-  msg ("closed file.");
+  msg ("close \"a\"");
 
-  invalidate_cache ();
+  CHECK ((fd = open ("a")) > 1, "open \"a\"");
 
-  fd = open("a");
-  ASSERT_CONDITION (fd > 2, "failed to open the file, expected a `fd` value greater than 2 but got: '%d'.", fd);
-  msg ("opened the file.");
-
-  for (size_t i = 0; i < FILE_SIZE / BLOCK_SIZE; i++)
-    {
-      res = read (fd, file_buffer, BLOCK_SIZE);
-      ASSERT_CONDITION (res == BLOCK_SIZE, "reading the file must read a block of size '%d', but got '%d'", FILE_SIZE, res);
-    }
-  msg ("read the file for the first time.");
-
-  close(fd);
-  msg ("closed file.");
+  CHECK (read (fd, buf, sizeof(buf)) == BUF_SIZE, "read from \"a\"");
 
   int first_hit = cache_stat (HIT);
   int first_miss = cache_stat (MISS);
-  msg ("stored hit and miss rate for the first read.");
 
-  fd = open("a");
-  ASSERT_CONDITION (fd > 2, "failed to open the file, expected a `fd` value greater than 2 but got: '%d'.", fd);
-  msg ("opened the file.");
+  close (fd);
+  msg ("close \"a\"");
 
-  for (size_t i = 0; i < FILE_SIZE / BLOCK_SIZE; i++)
-    {
-      res = read (fd, file_buffer, BLOCK_SIZE);
-      ASSERT_CONDITION (res == BLOCK_SIZE, "reading the file must read a block of size '%d', but got '%d'", FILE_SIZE, res);
-    }
-  msg ("read the file for the second time.");
+  CHECK ((fd = open ("a")) > 1, "open \"a\"");
 
-  close(fd);
-  msg ("closed file.");
+  CHECK (read (fd, buf, sizeof(buf)) == BUF_SIZE, "read from \"a\"");
 
   int second_hit = cache_stat (HIT);
   int second_miss = cache_stat (MISS);
-  msg ("stored hit and miss rate for the second read.");
 
-  
-  ASSERT_CONDITION (second_hit * (first_hit + first_miss) > first_hit * (second_hit + second_miss),
-          "hit rate must be larger for the second read.");
-  msg ("hit rate increased in the second read.");
+  CHECK (second_hit * (first_hit + first_miss) > first_hit * (second_hit + second_miss), "hit rate must increase");
 
-  res = remove ("a");
-  ASSERT_CONDITION (res > 0, "failed to remove the file.");
-  msg ("removed the file.");
+  msg ("close \"a\"");
+  close (fd);
 }
--- pintos/src/tests/filesys/extended/bf-hit.ck
+++ pintos/src/tests/filesys/extended/bf-hit.ck
@@ -4,20 +4,18 @@ use warnings;
 use tests::tests;
 check_expected (IGNORE_USER_FAULTS => 1, [<<'EOF']);
 (bf-hit) begin
-(bf-hit) created file named a.
-(bf-hit) opened the file.
-(bf-hit) wrote random bytes to file.
-(bf-hit) closed file.
-(bf-hit) opened the file.
-(bf-hit) read the file for the first time.
-(bf-hit) closed file.
-(bf-hit) stored hit and miss rate for the first read.
-(bf-hit) opened the file.
-(bf-hit) read the file for the second time.
-(bf-hit) closed file.
-(bf-hit) stored hit and miss rate for the second read.
-(bf-hit) hit rate increased in the second read.
-(bf-hit) removed the file.
+(bf-hit) invalidate cache
+(bf-hit) create "a"
+(bf-hit) open "a"
+(bf-hit) write to "a"
+(bf-hit) close "a"
+(bf-hit) open "a"
+(bf-hit) read from "a"
+(bf-hit) close "a"
+(bf-hit) open "a"
+(bf-hit) read from "a"
+(bf-hit) hit rate must increase
+(bf-hit) close "a"
 (bf-hit) end
 bf-hit: exit(0)
 EOF
